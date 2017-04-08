cae875f1a803073c6ffec17c10b774bf430a9753
Walter <relw1wa-github@yahoo.de>
2017-04-08 13:22:56 +0200

    Setup basic boilerplate

---
+++ src/components/t3-game.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class T3Game extends Component {
+  render() {
+    return (
+      <div className="t3-game">
+        <h1 className="text-center">
+          Tic Tac Toe Game
+        </h1>
+      </div>
+    );
+  }
+  
+};
+
+export default T3Game;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import T3Game from './components/t3-game';
+import styles from '../styles/style.scss';
+
+render(
+  <T3Game />,
+  document.getElementById('root')
+);
